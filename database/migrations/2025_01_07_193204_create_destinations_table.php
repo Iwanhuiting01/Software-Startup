13f0584c794bb9b1cff562163728676fa83f13e4
Iwanhuiting01 <i.l.huiting@st.hanze.nl>
2025-01-07 20:49:21 +0100

    Laravel setup met tailwind

--- database/migrations/2025_01_07_193204_create_destinations_table.php
+++ database/migrations/2025_01_07_193204_create_destinations_table.php
@@ -13,14 +13,15 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('destinations', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->boolean('is_employee');
-            $table->rememberToken();
+            $table->integer('required_people');
+            $table->integer('price');
+            $table->integer('vacation_category_id');
+            $table->string('destination');
+            $table->string('description');
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
             $table->timestamps();
         });
     }
@@ -32,6 +33,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('destinations');
     }
 };
